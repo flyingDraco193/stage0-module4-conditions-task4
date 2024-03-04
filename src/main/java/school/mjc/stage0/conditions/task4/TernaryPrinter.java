ea3ea3687ec44caab93d7808c646b9be7878d71f
Gempton <gemptonilya@gmail.com>
2024-03-04 20:04:15 +0300

    Done

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryPrinter.java
@@ -2,5 +2,7 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryPrinter {
     public void printWhichIsBigger(int first, int second) {
+        String greatestNumber = first > second ? "first" : "second";
+        System.out.println(greatestNumber);
     }
 }
